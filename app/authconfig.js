1bdc94e81feb7bcac1691e56a251baaa68ae3485
Jamie <jamie@tidepool.org>
2017-12-05 13:08:55 +1300

    blip clientID

--- app/authconfig.js
+++ app/authconfig.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'tidepool-dev.auth0.com',
-  clientId: '67bloa1Ib7dunN6cYcDTdffsYC0623O1',
+  clientId: 'uoaUqRCwDo4oEVF2gXwbaGMQ74GQYQLz',
   responseType: 'id_token token',
   scope: 'openid profile read:device-data',
   audience: 'open-api',
